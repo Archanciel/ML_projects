096a7c657d53596920ee90f48f3228583fe14325
Archanciel <jp.schnyder@gmail.com>
2019-05-29 17:20:21 +0200

    removed print out

--- Logistic regression/Iris FIsher/Octave/sigmoid.m
+++ Logistic regression/Iris FIsher/Octave/sigmoid.m
@@ -1,4 +1,4 @@
 function s = sigmoid(X, theta)
-    z = X * theta
-    s = 1 ./ (1 + exp(-z)) % pay attention to element wise division !
+    z = X * theta;
+    s = 1 ./ (1 + exp(-z)); % pay attention to element wise division !
 endfunction
