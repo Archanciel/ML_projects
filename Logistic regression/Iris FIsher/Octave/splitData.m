9bfcb19a9c5baf1cbadf6c23401c37e5e89a08f5
Archanciel <jp.schnyder@gmail.com>
2019-06-13 21:16:00 +0200

    add cell vector fill

--- Logistic regression/Iris FIsher/Octave/splitData.m
+++ Logistic regression/Iris FIsher/Octave/splitData.m
@@ -19,27 +19,23 @@ function [trainingDataCellVector, testingDataCellVector] = splitData(Data, m, yC
         % extracting training and testing data
         TrainingDataY1 = ClassDataY1(trainingDataIndexesY1,:)
         size(TrainingDataY1)
-        TestingDataY1 = ClassDataY1(testingDataIndexesY1,:)
-        size(TestingDataY1)
         TrainingDataY0 = ClassDataY0(trainingDataIndexesY0,:)
         size(TrainingDataY0)
+        TestingDataY1 = ClassDataY1(testingDataIndexesY1,:)
+        size(TestingDataY1)
         TestingDataY0 = ClassDataY0(testingDataIndexesY0,:)
         size(TestingDataY0)
         
         % setting y value to 1 or 0
         TrainingDataY1(:, yColIdx) = 1
-        TestingDataY1(:, yColIdx) = 1
         TrainingDataY0(:, yColIdx) = 0
+        TestingDataY1(:, yColIdx) = 1
         TestingDataY0(:, yColIdx) = 0
         
+        trainingDataCellVector{i} = [TrainingDataY1; TrainingDataY0]
+        testingDataCellVector{i} = [TestingDataY1; TestingDataY0]
     end
-    trainingDataCellVector{1} = [1 2;2 3];
-    trainingDataCellVector{2} = [1 2 3;2 3 4;5 6 7];
-    testingDataCellVector{1} = [10 20;20 30];
-    testingDataCellVector{2} = [10 20 30;20 30 40;50 60 70];
-    return
-    trainingDataIndexes = idx(1:round(p*m));
-    testingDataIndexes = idx(round(p*m) + 1:end);
-    TrainingData = D(trainingDataIndexes,:); 
-    TestingData = D(testingDataIndexes,:);
+    
+    % example unpacking the reurned data:
+    % cell2mat(trainingDataCellVector(1))
 endfunction
